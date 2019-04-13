3c7057eb4bd9b93316940bcae1629e463134bf91
shivangi14 <agshivangi14@gmail.com>
2019-04-13 14:31:07 +0530

    components sepearted into files

---
+++ src/components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function MyInfo(){
+  return(
+    <div>
+    <h1>Shivangi Agarwal</h1>
+    <p>Tech enthusiast</p>
+    <ol>
+      <li>andaman and nicobar</li>
+      <li>assam</li>
+      <li>himalayas</li>
+    </ol>
+    </div>
+  )
+}
+
+export default MyInfo
--- src/index.js
+++ src/index.js
@@ -1,51 +1,11 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-function MyInfo(){
-  return(
-    <div>
-    <h1>Shivangi Agarwal</h1>
-    <p>Tech enthusiast</p>
-    <ol>
-      <li>andaman and nicobar</li>
-      <li>assam</li>
-      <li>himalayas</li>
-    </ol>
-    </div>
-  )
-}
+import MyInfo from "./components/MyInfo"
 
 ReactDOM.render(<MyInfo />,document.getElementById("root"))
 
-/*
-function MyApp(){
-    return (
-    <ul>
-        <li>1</li>
-        <li>2</li>
-    </ul>
-    )
-}
-
-ReactDOM.render(
-    <MyApp />,
-    document.getElementById("root")
-    )
-
-*/
 
 
-/*
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
-*/
