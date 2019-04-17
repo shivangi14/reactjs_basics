ceb5b422ca2baebf1b5227cc54daef311c84e2d2
shivangi14 <agshivangi14@gmail.com>
2019-04-17 23:48:34 +0530

    state modification, class methods  and event handlers added

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,23 +1,30 @@
-import React from "react"
+import React,{Component} from "react"
 
 import MyInfo from "./MyInfo"
 import Joke from "./Joke"
 import jokesData from "./jokesData"
 
-function Main(){
-    const jokesComponents = jokesData.map(joke => {
-        const data = <div key={joke.id}>
-                        <Joke question={joke.question} punchLine={joke.punchLine} />
-                        <p>next...</p>
-                     </div>
-        return data
-    })
-    return(
-        <main>
-            <MyInfo />
-            {jokesComponents}
-        </main>
-    )
+class Main extends Component{
+    mapJokes(){
+        const jokesComponents = jokesData.map(joke => {
+                const data = <div key={joke.id}>
+                                <Joke question={joke.question} punchLine={joke.punchLine} />
+                                <p>next...</p>
+                             </div>
+                return data
+            })
+        return jokesComponents
+    }
+
+    render(){
+        let mappedJokes = this.mapJokes()
+        return(
+                <main>
+                    <MyInfo />
+                    {mappedJokes}
+                </main>
+            )
+    }
 }
 
 export default Main
--- src/components/MyInfo.js
+++ src/components/MyInfo.js
@@ -1,12 +1,29 @@
 import React from "react"
 
-function MyInfo(){
-  return(
-    <div>
-        <h1>Shivangi Agarwal</h1>
-        <p>Tech enthusiast</p>
-    </div>
-  )
+class MyInfo extends React.Component{
+    constructor(){
+        super()
+        this.state = {value :0}
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleClick(){
+        this.setState(prevState =>{
+            return {
+                value : prevState.value +1
+            }
+        })
+    }
+    render(){
+        return(
+            <div>
+                <h1>Shivangi Agarwal</h1>
+                <p>Tech enthusiast</p>
+                <span><button onClick={this.handleClick}>Click me!</button> You have already clicked me {this.state.value} times!</span>
+            </div>
+          )
+    }
+
 }
 
 export default MyInfo
