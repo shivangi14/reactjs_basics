80a983730db39f36f1faf34c78bd12e81b665db4
shivangi14 <agshivangi14@gmail.com>
2019-04-21 23:03:39 +0530

    added multi select checkbox

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,17 +5,28 @@ class Form extends Component{
         super()
         this.state={
             name :"",
-            age :18,
-            isPerson :false,
+            age :"",
             gender :"",
-            color : "green"
+            color : "",
+            diet:{
+                veg:false,
+                nonVeg:false,
+                egg:false
+            }
         }
         this.handleChange = this.handleChange.bind(this)
     }
 
     handleChange(event){
         const {name,value,checked,type} = event.target
-        type === "checkbox"? this.setState({ [name]:checked}) : this.setState({[name] : value})
+        type === "checkbox"? this.setState(prevState=>{
+            return{
+                diet:{
+                    ...prevState.diet,
+                    [name] : checked
+                }
+            }
+        }) : this.setState({[name] : value})
 
     }
 
@@ -32,17 +43,33 @@ class Form extends Component{
                 <br/>
                 <input type="number" placeholder="Enter your age" value={this.state.age} name="age" onChange={this.handleChange}/>
                 <br/>
-                <input type="checkbox" checked={this.state.isPerson} name="isPerson" onChange={this.handleChange} />Are you a person?
+                choose diet<br/>
+                <input type="checkbox" checked={this.state.diet.veg} name="veg" onChange={this.handleChange} />veg
                 <br/>
+                <input type="checkbox" checked={this.state.diet.nonVeg} name="nonVeg" onChange={this.handleChange} />non veg
+                <br/>
+                <input type="checkbox" checked={this.state.diet.egg} name="egg" onChange={this.handleChange} />egg
+                <br/>
+                Gender 
                 <input type="radio" name="gender" value="male" checked={this.state.gender === "male"} onChange={this.handleChange} />Male
                 <input type="radio" name="gender" value="female" checked={this.state.gender === "female"} onChange={this.handleChange} />Female
                 <br/>
                 Fav color <select value={this.state.color} name="color" onChange={this.handleChange}>
+                    <option value="">Select your color</option>
                     <option value="blue">Blue</option>
                     <option value="red">Red</option>
                     <option value="green">Green</option>
                 </select>
+                <br/>
                 <button>Submit</button>
+                <br/>
+                Data you submitted-
+                <br/>
+                your name : {this.state.name}<br/>
+                your age : {this.state.age}<br/>
+                gender : {this.state.gender}<br/>
+                diet : {this.state.diet.veg && "veg"} {this.state.diet.nonVeg && "non veg"} {this.state.diet.egg && "egg"}<br/>
+                fav color : {this.state.color}<br/>
                 <hr/>
             </form>
         )
